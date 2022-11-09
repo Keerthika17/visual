b1d06d36e93c5d8dbff2cb7ec95c062c7e5c82e5
Keerthikasubramaniyam <keerthikasubramaniyam2001@gmail.com>
2022-11-09 16:56:35 +0530

    add

---
+++ src/components/columns.js
@@ -0,0 +1,22 @@
+export const COLUMNS = [
+  {
+    Header: "ID",
+    accessor: "id",
+  },
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "QUANTITY",
+    accessor: "quantitiy",
+  },
+  {
+    Header: "SALES",
+    accessor: "sales",
+  },
+  {
+    Header: "ORDERED",
+    accessor: "ordered",
+  },
+];
